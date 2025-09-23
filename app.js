94bc007b5dc5b76b28eb28af39704ed22f00f55a
Mariana weigert <mari_weigert@hotmail.com>
2025-09-23 14:36:30 -0300

    add lógica do projeto

--- app.js
+++ app.js
@@ -1,1 +1,51 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaAmigos = [];
+
+function adicionarAmigo() {
+    limparResultado();
+
+    const nomeAmigoTela = document.getElementById("amigo");
+    const nomeAmigo = nomeAmigoTela.value.trim();
+
+    if(nomeAmigo == "") {
+        alert("Por favor insira o nome!");
+        return;
+    }
+
+    listaAmigos.push(nomeAmigo);
+
+    const listaAmigosTela = document.getElementById("listaAmigos");
+
+    const itemLista = document.createElement("li");
+    itemLista.textContent = nomeAmigo;
+
+    listaAmigosTela.append(itemLista);
+
+    nomeAmigoTela.value = "";
+}
+
+function sortearAmigo() {
+    if(listaAmigos.length == 0) {
+        alert("Por favor insira pelo menos um nome!");
+        return;
+    }
+
+    const indiceAmigoSorteado = Math.trunc(Math.random() * listaAmigos.length);
+    const amigoSorteado = listaAmigos[indiceAmigoSorteado];
+
+    limparLista();
+
+    const resultadoTela = document.getElementById("resultado");
+    resultadoTela.textContent = "O seu amigo sorteado é: " + amigoSorteado;
+}
+
+function limparLista() {
+    listaAmigos = [];
+    const listaAmigosTela = document.getElementById("listaAmigos");
+    listaAmigosTela.innerHTML = "";
+}
+
+function limparResultado() {
+    const resultadoTela = document.getElementById("resultado");
+    resultadoTela.innerHTML = "";
+}
